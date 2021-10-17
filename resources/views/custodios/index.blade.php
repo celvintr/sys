2c481247336a58cbbb0620dccde46ebd187d8aa0
celvintr <turciosr1991@gmail.com>
2021-10-17 16:37:30 -0600

    traduccion

--- resources/views/custodios/index.blade.php
+++ resources/views/custodios/index.blade.php
@@ -45,20 +45,14 @@
                                 <ul class="navi flex-column navi-hover py-2">
                                     <li
                                         class="navi-header font-weight-bolder text-uppercase font-size-sm text-primary pb-2">
-                                        Choose an option:
+                                        Herramientas:
                                     </li>
                                     <li class="navi-item">
                                         <a href="#" class="navi-link">
                                             <span class="navi-icon"><i class="la la-print"></i></span>
-                                            <span class="navi-text">Print</span>
+                                            <span class="navi-text">Imprimir</span>
                                         </a>
-                                    </li>
-                                    <li class="navi-item">
-                                        <a href="#" class="navi-link">
-                                            <span class="navi-icon"><i class="la la-copy"></i></span>
-                                            <span class="navi-text">Copy</span>
-                                        </a>
-                                    </li>
+                                    </li>                              
                                     <li class="navi-item">
                                         <a href="#" class="navi-link">
                                             <span class="navi-icon"><i
